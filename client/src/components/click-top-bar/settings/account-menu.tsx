8689ea8ae2e9e70d74ae44150aa8c9c2565e4987
sasha kushnir <sasha@make.sevices>
2025-12-01 13:28:01 +0200

    updated topbar. Added new items into account dropdown

--- client/src/components/click-top-bar/settings/account-menu.tsx
+++ client/src/components/click-top-bar/settings/account-menu.tsx
@@ -6,39 +6,39 @@ import DocsIcon from 'assets/logos/docs.svg';
 const openInNewTab = (url: string) => () => window.open(url, '_blank');
 
 type MenuItemConfig = {
-    label: string;
-    url: string;
-    badge?: BadgeProps,
-    icon?: any;
+  label: string;
+  url: string;
+  badge?: BadgeProps;
+  icon?: any;
 };
 
 const menuConfig: MenuItemConfig[] = [
-    {
-        label: 'CSPR.click docs',
-        url: 'https://docs.cspr.click',
-        icon: CSPRClickIcon,
-    },
-    {
-        label: 'CSPR.cloud docs',
-        url: 'https://docs.cspr.cloud',
-        icon: CloudIcon
-    },
-    {
-        label: 'Odra docs',
-        url: 'https://odra.dev/docs',
-        icon: DocsIcon
-    }
+  {
+    label: 'CSPR.click docs',
+    url: 'https://docs.cspr.click',
+    icon: CSPRClickIcon
+  },
+  {
+    label: 'CSPR.cloud docs',
+    url: 'https://docs.cspr.cloud',
+    icon: CloudIcon
+  },
+  {
+    label: 'Odra docs',
+    url: 'https://odra.dev/docs',
+    icon: DocsIcon
+  }
 ];
 
 export const accountMenuItems = [
-    <AccountCardMenuItem key="account-card" />,
-    ...menuConfig.map((item: MenuItemConfig, index: number) => (
-        <AccountMenuItem
-            key={index}
-            onClick={openInNewTab(item.url)}
-            icon={item.icon}
-            label={item.label}
-            {...(item.badge && { badge: item.badge })}
-        />
-    ))
+  <AccountCardMenuItem key="account-card" />,
+  ...menuConfig.map((item: MenuItemConfig, index: number) => (
+    <AccountMenuItem
+      key={index}
+      onClick={openInNewTab(item.url)}
+      icon={item.icon}
+      label={item.label}
+      {...(item.badge && { badge: item.badge })}
+    />
+  ))
 ];
