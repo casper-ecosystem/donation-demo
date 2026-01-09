d6610b4a756b4bb7ea591c066dcd31df4644be44
David Hernando <david.hernando@make.services>
2026-01-09 10:06:48 +0100

    revert api url change and keep local url

--- client/public/config.js
+++ client/public/config.js
@@ -1,7 +1,7 @@
 const config = {
   donation_contract_package_hash:
     "c447e9d334a710bc3e0a47cbea854c269e41637d7b9aa9d37a745596f651ed7a",
-  donation_api_url: "https://donation-demo-api.casper.network",
+  donation_api_url: "http://localhost:4000",
   cspr_click_app_name: "Donation Demo",
   cspr_click_app_id: "csprclick-template",
   cspr_click_providers: ['casper-wallet', 'ledger', 'metamask-snap'],
