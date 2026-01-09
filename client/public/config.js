e0a2934981c9c6e38127b3bb81c7a75f03727cad
David Hernando <david.hernando@make.services>
2026-01-09 09:58:48 +0100

    Use transaction.toJSON() as argument to csprclick.send() Use correct contract hash for testnet. Remove csprclick event listeners when App is unmounted.

--- client/public/config.js
+++ client/public/config.js
@@ -1,7 +1,7 @@
 const config = {
   donation_contract_package_hash:
-    "ca0f4eedc84e03b6bc39ce664ef05dff00a96214194e706d50bfc43d84124035",
-  donation_api_url: "http://localhost:4000",
+    "c447e9d334a710bc3e0a47cbea854c269e41637d7b9aa9d37a745596f651ed7a",
+  donation_api_url: "https://donation-demo-api.casper.network",
   cspr_click_app_name: "Donation Demo",
   cspr_click_app_id: "csprclick-template",
   cspr_click_providers: ['casper-wallet', 'ledger', 'metamask-snap'],
